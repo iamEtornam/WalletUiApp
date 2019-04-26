21462ecfe928859785c138947623789a3a9919e4
Etornam Sunu <sunumacbright@gmail.com>
2019-04-26 20:47:43 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,95 @@
+# wallet_ui_app
+
+<p align="center">
+    
+[![Maintenance](https://img.shields.io/badge/Maintained%3F-yes-green.svg)](https://github.com/RegNex/WalletUiApp/graphs/commit-activity)
+[![ForTheBadge built-with-love](http://ForTheBadge.com/images/badges/built-with-love.svg)](https://github.com/RegNex/)
+</p>
+
+This project is an implementation of a Design i found on [Uplabs.com](https://www.uplabs.com/posts/wallet-app-iphones)
+
+## Art
+
+<img align="left" src="https://github.com/RegNex/WalletUiApp/blob/master/screenshot/art_1.png" width="200" height="400"/>
+<img src="https://github.com/RegNex/WalletUiApp/blob/master/screenshot/art_2.png" width="200" height="400"/>
+
+## Getting Started
+
+This project is a starting point for a Flutter application.
+
+To clone this project,
+open your terminal or cmd
+
+```
+cd folder/to/clone-into/
+```
+
+```
+git clone https://github.com/RegNex/WalletUiApp.git
+```
+
+Then
+locate the project on your system and open with android studio or Vscode or intellij IDE.
+
+To Run:
+
+```
+C:\path\to\project> flutter run
+
+```
+
+A few resources to get you started if this is your first Flutter project:
+
+- [Lab: Write your first Flutter app](https://flutter.io/docs/get-started/codelab)
+- [Cookbook: Useful Flutter samples](https://flutter.io/docs/cookbook)
+
+For help getting started with Flutter, view our
+[online documentation](https://flutter.io/docs), which offers tutorials,
+samples, guidance on mobile development, and a full API reference.
+
+## Prerequisites
+
+What things you need to run the app
+
+```
+* Android Studio/Vscode/Intellij IDE
+* Flutter SDK
+* Android SDK
+```
+
+## How to contribute
+
+- **Fork the repository and clone it locally**. Connect your local to the original “upstream” repository by adding it as a remote. Pull in changes from “upstream” often so that you stay up to date so that when you submit your pull request, merge conflicts will be less likely. (See more detailed instructions here.)
+- **Create a branch** for your edits.
+- **Reference any relevant issues** or supporting documentation in your PR (for example, “Closes #37.”)
+- **Include screenshots of the before and after** if your changes include differences in HTML/CSS. Drag and drop the images into the body of your pull request.
+- **Test your changes!** Run your changes against any existing tests if they exist and create new ones when needed. Whether tests exist or not, make sure your changes don’t break the existing project.
+- **Contribute in the style of the project** to the best of your abilities. This may mean using indents, semi-colons or comments differently than you would in your own repository, but makes it easier for the maintainer to merge, others to understand and maintain in the future.
+
+## Built With
+
+- [Android Studio](https://developer.android.com/studio/install) - How to install Android Studio
+- [Flutter](https://flutter.io) - Flutter Official website
+
+## Author
+
+**Etornam Sunu Bright**
+
+- [**Twitter**](https://twitter.com/_regnex)
+- [**Linkedin**](https://linkedin.com/in/etornam-sunu)
+
+## License
+
+Copywrite 2018 Sunu Bright Etornam
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
---
+++ lib/main.dart
@@ -0,0 +1,244 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Wallet',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: DashBoardPage(),
+    );
+  }
+}
+
+class DashBoardPage extends StatefulWidget {
+  @override
+  _DashBoardPageState createState() => _DashBoardPageState();
+}
+
+class _DashBoardPageState extends State<DashBoardPage> {
+  List<Color> _backgroundColor;
+  Color _iconColor;
+  Color _textColor;
+  List<Color> _actionContainerColor;
+  Color _borderContainer;
+  bool colorSwitched = false;
+  var logoImage;
+
+  void changeTheme() async {
+    if (colorSwitched) {
+      setState(() {
+        logoImage = 'assets/images/wallet_dark_logo.png';
+        _backgroundColor = [
+          Color.fromRGBO(255, 212, 61, 1),
+          Color.fromRGBO(255, 212, 55, 1),
+          Color.fromRGBO(255, 211, 48, 1),
+          Color.fromRGBO(255, 211, 43, 1),
+        ];
+        _iconColor = Colors.white;
+        _textColor = Color.fromRGBO(253, 211, 4, 1);
+        _borderContainer = Color.fromRGBO(34, 58, 90, 0.2);
+        _actionContainerColor = [
+          Color.fromRGBO(47, 75, 110, 1),
+          Color.fromRGBO(43, 71, 105, 1),
+          Color.fromRGBO(39, 64, 97, 1),
+          Color.fromRGBO(34, 58, 90, 1),
+        ];
+      });
+    } else {
+      setState(() {
+        logoImage = 'assets/images/wallet_logo.png';
+        _borderContainer = Color.fromRGBO(252, 233, 187, 1);
+        _backgroundColor = [
+          Color.fromRGBO(249, 249, 249, 1),
+          Color.fromRGBO(241, 241, 241, 1),
+          Color.fromRGBO(233, 233, 233, 1),
+          Color.fromRGBO(222, 222, 222, 1),
+        ];
+        _iconColor = Colors.black;
+        _textColor = Colors.black;
+        _actionContainerColor = [
+          Color.fromRGBO(255, 212, 61, 1),
+          Color.fromRGBO(255, 212, 55, 1),
+          Color.fromRGBO(255, 211, 48, 1),
+          Color.fromRGBO(255, 211, 43, 1),
+        ];
+      });
+    }
+  }
+
+  @override
+  void initState() {
+    changeTheme();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: Container(
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width,
+          decoration: BoxDecoration(
+              gradient: LinearGradient(
+                  begin: Alignment.centerLeft,
+                  end: Alignment.centerRight,
+                  stops: [0.2, 0.4, 0.6, 0.8],
+                  colors: _backgroundColor)),
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Align(
+                alignment: Alignment.centerRight,
+                child: Switch.adaptive(
+                  onChanged: (value) {
+                    if (colorSwitched) {
+                      colorSwitched = false;
+                    } else {
+                      colorSwitched = true;
+                    }
+                    changeTheme();
+                  },
+                  value: colorSwitched,
+                ),
+              ),
+              Image.asset(
+                logoImage,
+                fit: BoxFit.contain,
+                height: 100.0,
+                width: 100.0,
+              ),
+              Column(
+                children: <Widget>[
+                  Text(
+                    'Hello',
+                    style: TextStyle(fontSize: 18, color: Colors.black),
+                  ),
+                  Text(
+                    'James Cashman',
+                    style: TextStyle(
+                        fontSize: 24,
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold),
+                  )
+                ],
+              ),
+              Container(
+                height: 300.0,
+                width: MediaQuery.of(context).size.width,
+                decoration: BoxDecoration(
+                    color: _borderContainer,
+                    borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(15),
+                        topRight: Radius.circular(15))),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(15),
+                            topRight: Radius.circular(15)),
+                        gradient: LinearGradient(
+                            begin: Alignment.centerLeft,
+                            end: Alignment.centerRight,
+                            stops: [0.2, 0.4, 0.6, 0.8],
+                            colors: _actionContainerColor)),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Container(
+                          height: 70,
+                          child: Center(
+                            child: ListView(
+                              children: <Widget>[
+                                Text(
+                                  '790',
+                                  textAlign: TextAlign.center,
+                                  style: TextStyle(
+                                      color: _textColor,
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 30),
+                                ),
+                                Text(
+                                  'Available Cash',
+                                  textAlign: TextAlign.center,
+                                  style: TextStyle(
+                                      color: _iconColor, fontSize: 16),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                        Divider(
+                          height: 0.5,
+                          color: Colors.grey,
+                        ),
+                        Table(
+                          border: TableBorder.symmetric(
+                            inside: BorderSide(
+                                color: Colors.grey,
+                                style: BorderStyle.solid,
+                                width: 0.5),
+                          ),
+                          children: [
+                            TableRow(children: [
+                              _actionList(
+                                  'assets/images/ic_send.png', 'Send Money'),
+                              _actionList(
+                                  'assets/images/ic_money.png', 'Request'),
+                            ]),
+                            TableRow(children: [
+                              _actionList('assets/images/ic_transact.png',
+                                  'Transactions'),
+                              _actionList('assets/images/ic_reward.png',
+                                  'Reward Points'),
+                            ])
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _actionList(var icon, String desc) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Image.asset(
+            icon,
+            fit: BoxFit.contain,
+            height: 45.0,
+            width: 45.0,
+            color: _iconColor,
+          ),
+          SizedBox(
+            height: 8,
+          ),
+          Text(
+            desc,
+            style: TextStyle(color: _iconColor),
+          )
+        ],
+      ),
+    );
+  }
+}
